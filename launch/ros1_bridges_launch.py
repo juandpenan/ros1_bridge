d3c34810c82beb5aa61f2750d9e74654c092ccd3
dsobh <davidsobrin@gmail.com>
2023-02-15 12:06:59 +0100

    Error fixed in launch.py

--- launch/ros1_bridges_launch.py
+++ launch/ros1_bridges_launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
 	"imu": "simple_bridge_1_to_2_imu",
 	"range": "simple_bridge_1_to_2_sonar",
 	"compressed": "simple_bridge_1_to_2_compressed",
-	"point_cloud2": "simple_bridge_1_to_2_point_cloud2"}}
+	"point_cloud2": "simple_bridge_1_to_2_point_cloud2"}
 	
 	data = open_yaml_file()
 
