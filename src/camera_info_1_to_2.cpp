098e2e1079cf99e655db6eb928e19989549b58fc
Juancams <jc.manzanares.serrano@gmail.com>
2023-02-21 19:22:11 +0100

    Added camera_info bridge

---
+++ src/camera_info_1_to_2.cpp
@@ -0,0 +1,141 @@
+// Copyright 2015 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iostream>
+#include <memory>
+#include <utility>
+
+// include ROS 1
+#ifdef __clang__
+# pragma clang diagnostic push
+# pragma clang diagnostic ignored "-Wunused-parameter"
+#endif
+#include "ros/ros.h"
+#include "sensor_msgs/CameraInfo.h"
+#ifdef __clang__
+# pragma clang diagnostic pop
+#endif
+
+// include ROS 2
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/camera_info.hpp"
+#include "rclcpp/publisher.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "lifecycle_msgs/msg/transition.hpp"
+
+rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::CameraInfo>::SharedPtr pub;
+
+class LifecycleNode: public rclcpp_lifecycle::LifecycleNode
+{
+public:
+  explicit LifecycleNode(const std::string & node_name, bool intra_process_comms = false)
+  : rclcpp_lifecycle::LifecycleNode(node_name,
+      rclcpp::NodeOptions().use_intra_process_comms(intra_process_comms))
+  {
+    pub = this->create_publisher<sensor_msgs::msg::Image>("/xtion/rgb/camera_info", 100);
+  }
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_configure(const rclcpp_lifecycle::State &)
+  {
+    RCLCPP_INFO(get_logger(), "on_configure()");
+
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  }
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_activate(const rclcpp_lifecycle::State &)
+  {
+    pub->on_activate();
+
+    RCLCPP_INFO(get_logger(), "on_activate()");
+
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  }
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_deactivate(const rclcpp_lifecycle::State &)
+  {
+    pub->on_deactivate();
+
+    RCLCPP_INFO(get_logger(), "on_deactivate()");
+
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  }
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_cleanup(const rclcpp_lifecycle::State &)
+  {
+    pub.reset();
+
+    RCLCPP_INFO(get_logger(), "on cleanup()");
+
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  }
+
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_shutdown(const rclcpp_lifecycle::State &)
+  {
+    pub.reset();
+
+    return rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+  }
+
+  friend void Callback(boost::shared_ptr<sensor_msgs::Image> ros1_msg);
+};
+
+void Callback(boost::shared_ptr<sensor_msgs::CameraInfo> ros1_msg)
+{
+  if (pub->get_subscription_count() == 0)
+    return;
+
+  auto ros2_msg = std::make_unique<sensor_msgs::msg::CameraInfo>();
+
+  ros2_msg->header.frame_id = ros1_msg->header.frame_id;
+  ros2_msg->header.stamp = rclcpp::Time(ros1_msg->header.stamp.toNSec());
+
+  ros2_msg->height = ros1_msg->height;
+  ros2_msg->width = ros1_msg->width;
+  ros2_msgs->distortion_model = ros1_msg->distortion_model;
+  ros2_msgs->D = std::move(ros1_msg->D);
+  ros2_msgs->K = std::move(ros1_msg->K);
+  ros2_msgs->R = std::move(ros1_msg->R);
+  ros2_msgs->P = std::move(ros1_msg->P);
+  ros2_msgs->binning_x = ros1_msg->binning_x;
+  ros2_msgs->binning_y = ros1_msg->binning_y;
+  ros2_msgs->roi = ros1_msg->roi;
+
+  pub->publish(std::move(ros2_msg));
+}
+
+int main(int argc, char * argv[])
+{
+  // ROS 2 node and publisher
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<LifecycleNode>("camera_info_1_to_2");
+
+  // ROS 1 node and subscriber
+  ros::init(argc, argv, "image_1_to_2");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("/xtion/rgb/camera_info", 100, TFCallback);
+
+  while (rclcpp::ok() && ros::ok()) {
+    ros::spinOnce();
+    rclcpp::spin_some(node->get_node_base_interface());
+  }
+
+  rclcpp::shutdown();
+
+  return 0;
+}
